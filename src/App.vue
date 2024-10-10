7d122303c4dd7d86d106ed2319ce5e2c58515ffa
trane7776 <izbasarov.t@edu.rea.ru>
2024-10-10 16:35:49 +0300

    loops and conditions

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@ export default {
   data() {
     return {
       name: 'John Snow',
-      status: true,
+      status: 'active',
+      tasks: ['Task One', 'Task Two', 'Task Three'],
     };
   },
 };
@@ -11,7 +12,13 @@ export default {
 
 <template>
   <h1>{{ name }}</h1>
-  <p v-if="status">User is active</p>
+  <p v-if="status === 'active'">User is active</p>
+  <p v-else-if="status === 'pending'">User is pending</p>
+  <p v-else>User is inactive</p>
+  <h3>Tasks:</h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul>
 </template>
 
 <style scoped></style>

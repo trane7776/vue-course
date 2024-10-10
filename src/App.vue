71251554e86158bfbb74ddbc07516cebc148f089
trane7776 <izbasarov.t@edu.rea.ru>
2024-10-10 16:29:56 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  data() {
+    return {
+      name: 'John Snow',
+      status: true,
+    };
+  },
+};
+</script>
+
+<template>
+  <h1>{{ name }}</h1>
+  <p v-if="status">User is active</p>
+</template>
+
+<style scoped></style>
---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { fileURLToPath, URL } from 'node:url';
+
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    port: 3000, // Choose any available port
+  },
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
